a657156095bdb90e5bdb5c967bb1a819758820c6
elazar1444 <elazar1444@gmail.com>
2023-06-05 21:55:51 +0300

    fiver ver1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { useState } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
+
 function App() {
+
+  const [show,setShow]=useState(false);
+
+//   // רשימת המשתמשים
+// const [users,setUsers]=useState([]);
+// // הצגת התפריט לאחר כניסה או הרשמה
+// const [show,setShow]=useState(false);
+
+// הצגת תפריט כפתורים
+const showNavbar =()=>{
+  if(show){
+    return <Navbar/>
+  }
+}
+
+
+
+
+// //                   הוספת פוסט חדש למשתמש
+// // מגיע לפונקציה גם האינדקס של המשתמש בכדי לדעת היכן להוסיף את הפוסטים
+// const addPost =(index,body,year)=>{
+//   let temp = {body:body,year:year};
+//   users[index].postsOfUser.push(temp)
+//   setUsers([...users])
+
+// }
+
+
+
+// // הוספת משתמש
+// const addNewUser =(name,age)=>{
+//   setUsers([...users,{nameOfUser:name , ageOfUser:age,  postsOfUser:[]}])
+// }
+
+
+// // עריכת המשתמש על ידי קבלת המיקום שבו נמצא - אינדקס 
+// const editAgeOfUser=(index,age)=>{
+//   users[index].ageOfUser=age;
+//   setUsers([...users])
+// }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+    <BrowserRouter>
+{showNavbar()}
+    <Routes>
+  
+    <Route/>
+   
+
+    </Routes>
+    
+    </BrowserRouter>
+
+
+
+
+
+
     </div>
   );
 }
